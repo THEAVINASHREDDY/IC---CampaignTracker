def761d23017fe5eabbbc9d38ae8bf62cae8a507
Kunjchacha <bornstudk@gmail.com>
2025-01-16 16:53:12 +0530

    Update CampaignTracker.py

--- CampaignTracker.py
+++ CampaignTracker.py
@@ -9,8 +9,9 @@ campaigns = [
         "name": "Access Group",
         "weeklyTarget": 10,
         "cumulativeTarget": 100,
+        "Till 15th Jan": 64,
         "weeks": [
-            {"week": 1, "focus": "Till 10th Jan", "target": 0, "actual": 60},
+            {"week": 1, "focus": "Till 15th Jan", "target": 0, "actual": 64},
             {"week": 2, "focus": "3 emails/week + LinkedIn (13th - 17th Jan)", "target": 10, "actual": 4},
             {"week": 3, "focus": "3 emails/week + LinkedIn (20th - 24th Jan)", "target": 10, "actual": 0},
             {"week": 4, "focus": "3 emails/week + LinkedIn (27th - 31st Jan)", "target": 10, "actual": 0},
@@ -89,10 +90,8 @@ df = pd.DataFrame(data)
 
 # Display summary
 actual, target = calculate_progress(selected_campaign["weeks"])
-week = campaigns[0]["weeks"][0]["focus"]
-cumulativeTarget = campaigns[0]["cumulativeTarget"]
 st.markdown(f"### {selected_campaign_name}")
-st.write(f"Weekly Target: {selected_campaign['weeklyTarget']} | {week}:{actual} | Progress: {actual}/{cumulativeTarget}")
+st.write(f"Weekly Target: {selected_campaign['weeklyTarget']} | Progress: {actual}/{target}")
 
 # Line chart
 fig = go.Figure()
